adf04208cfbbf18564947198198d1cfc8f6b5c32
LuisMagana <2017luismagana@gmail.com>
2020-01-10 16:42:31 -0800

    changing screens

--- AwesomeProject/screens/LinksScreen.js
+++ AwesomeProject/screens/LinksScreen.js
@@ -1,99 +1,198 @@
-import * as React from 'react';
-import { StatusBar, View, TextInput, Text, FlatList, TouchableOpacity  } from 'react-native';
-import { LinearGradient } from 'expo';
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-export default class LinksScreen extends React.Component {
-  state = {
-    inputValue: '',
-    todos: []
-  };
+import { MonoText } from '../components/StyledText';
 
-  changeText = value => {
-    this.setState({
-      inputValue: value
-    });
-  };
-
-  addItem = () => {
-    if (this.state.inputValue !== '') {
-      this.setState(prevState => {
-        const newToDo = {
-          title: this.state.inputValue,
-          createdAt: Date.now(),
-        };
-
-        var todos = this.state.todos.concat(newToDo);
-
-        this.setState({
-          inputValue: '',
-          todos: todos
-        });
-      });
-    }
-  };
-
-  render() {
-    const todos = this.state.todos.reverse().map((todo, key) =>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 20 }}>
-
-        <View style={{ flexDirection: 'row' }}>
-          <TouchableOpacity style={{
-            width: 20,
-            height: 20,
-            borderRadius: 15,
-            borderWidth: 3,
-            borderColor: 'white',
-            margin: 15
-          }}>
-          </TouchableOpacity>
-          <Text style={{ paddingLeft: 5, marginTop:10, fontSize: 28, color: 'white'}}>{todo.title}</Text>
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <View style={styles.welcomeContainer}>
+          <Image
+            source={
+              __DEV__
+                ? require('../assets/images/robot-dev.png')
+                : require('../assets/images/robot-prod.png')
+            }
+            style={styles.welcomeImage}
+          />
         </View>
 
-        <View>
-          <TouchableOpacity>
-            <Text style={{ color: 'white', fontSize: 28, marginRight: 15 }}>x</Text>
+        <View style={styles.getStartedContainer}>
+          <DevelopmentModeNotice />
+
+          <Text style={styles.getStartedText}>Get started by opening</Text>
+
+          <View
+            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+            <MonoText>screens/HomeScreen.js</MonoText>
+          </View>
+
+          <Text style={styles.getStartedText}>
+            Ok so it works now haha
+          </Text>
+        </View>
+
+        <View style={styles.helpContainer}>
+          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
+            <Text style={styles.helpLinkText}>
+              Help, it didn’t automatically reload!
+            </Text>
           </TouchableOpacity>
         </View>
+      </ScrollView>
+
+      <View style={styles.tabBarInfoContainer}>
+        <Text style={styles.tabBarInfoText}>
+          This is a tab bar. You can edit it in:
+        </Text>
+
+        <View
+          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+          <MonoText style={styles.codeHighlightText}>
+            navigation/MainTabNavigator.js
+          </MonoText>
+        </View>
+      </View>
     </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+function DevelopmentModeNotice() {
+  if (__DEV__) {
+    const learnMoreButton = (
+      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
+        Learn more
+      </Text>
     );
 
     return (
-      <LinearGradient colors={['#667eea', '#764ba2']} style={{ flex: 1 }}>
-        <StatusBar barStyle="light-content" />
-        <View>
-          <TextInput
-            style={styles.input}
-            onSubmitEditing={this.addItem}
-            onChangeText={this.changeText}
-            placeholder="Type here to add a to do."
-            value={this.state.inputValue}
-            placeholderTextColor={'#fff'}
-            multiline={true}
-            autoCapitalize="sentences"
-            underlineColorAndroid="transparent"
-            selectionColor={'white'}
-            maxLength={30}
-            returnKeyType="done"
-            autoCorrect={false}
-            blurOnSubmit={true}
-          />
-        </View>
-        <View>
-        {todos}
-        </View>
-      </LinearGradient>
+      <Text style={styles.developmentModeText}>
+        Development mode is enabled: your app will be slower but you can use
+        useful development tools. {learnMoreButton}
+      </Text>
+    );
+  } else {
+    return (
+      <Text style={styles.developmentModeText}>
+        You are not in development mode: your app will run at full speed.
+      </Text>
     );
   }
 }
 
-const styles = {
-  input: {
-    marginTop: 30,
-    paddingTop: 10,
-    paddingRight: 15,
-    paddingLeft: 15,
-    fontSize: 34,
-    color: 'white',
-    fontWeight: '500',
+function handleLearnMorePress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/development-mode/'
+  );
+}
+
+function handleHelpPress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
-};
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
