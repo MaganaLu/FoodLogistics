a0421262089d4e7878966d9e4f4a05006716196f
msolwold <msolwold35@gmail.com>
2020-01-21 15:57:24 -0800

    removed redundant component

--- src/default/Frames/Frame_Body.tsx
+++ src/default/Frames/Frame_Body.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-import AppBody from "../../body/FoodLogisticsBody";
+import Home from '../../body/FoodLogisticsBody_Home';
+import TestDB from '../../../storage/testDB'
+import Activity from '../../body/FoodLogisticsBody_Activity';
+import Map from '../../body/FoodLogisticsBody_Map';
+import Account from '../../body/FoodLogisticsBody_Account';
 
 type Page = "main" | "activity" | "map" | "account";
 
 type Props = {
 	isLoggedIn: true | false;
-	page: Page
+	page: Page;
 };
 
 type State = {
@@ -23,20 +27,35 @@ export default class Frame_Body extends React.Component<Props, State>
 	}
 
 	render() {
-		return (
-			<View style={styles.body}>
-          <AppBody page={this.props.page} />
-			</View>
-		)
+		if (this.props.page == "main") {
+            return (
+                <Home/>
+            )
+        }
+
+        else if (this.props.page == "activity") {
+            return (
+                <Activity/>
+            )
+        }
+
+        else if (this.props.page == "map") {
+            return (
+                <Map/>
+            )
+        }
+
+        else if (this.props.page == "account") {
+            return (
+                <Account/>
+            )
+        }
+
+        else {
+            return (
+                <Text>Ahh it broke!</Text>
+            )
+        }
 	}
 }
-
-const styles = StyleSheet.create(
-{
-    body: {
-      flex: 8,
-      justifyContent: "center",
-      alignSelf: "center"
-    }
-});
   
